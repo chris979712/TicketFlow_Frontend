dd112793accbc426a4dec6aa30148fd1c3c97794
HizayAP <hizayapodaca@gmail.com>
2025-11-04 18:12:56 -0600

    Cambio en logo login

--- TicketFlow_Frontend/src/features/login/pages/login.tsx
+++ TicketFlow_Frontend/src/features/login/pages/login.tsx
@@ -1,16 +1,16 @@
 import { LoginForm } from "../components/LoginForm"
-import ticketFlowLogo from '../../../assets/TicketFlow_horizontal.png';
+import ticketFlow_horizontal from '../../../assets/TicketFlow_horizontal.png';
 import './login.css'
 
 export default function Login(){
     return(
         <div className="login">
             <section className="welcome-messages-section">
+                <img src ={ticketFlow_horizontal} alt="TicketFlowH" title="TicketFlowH" className="img_logo_horizontal"/>
                 <h2>Bienvenido</h2>
                 <p className="p_welcomeMessage">Descubre eventos, recibe alertas sobre tus artistas favoritos, obras de teatro y más. Además de la impresión de tus boletos de forma rápida y segura.</p>
             </section>
             <section className="register-section">
-                <img src={ticketFlowLogo} alt="TicketFlow logo" title="TicketFlow Logo" className="img_logo"/>
                 <h2>Iniciar sesión</h2>
                 <p className="p_indications"><strong>Si no tiene una cuenta, de click en registrase.</strong></p>
                 <LoginForm />
