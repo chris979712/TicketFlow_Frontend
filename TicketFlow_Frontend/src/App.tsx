8edeecd4318df498c4ae6153453e822c28574f24
chris979712 <chrisvasquez985@gmail.com>
2025-12-04 15:03:46 -0600

    Configuración para el despliegue en Vercel

--- TicketFlow_Frontend/src/App.tsx
+++ TicketFlow_Frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import { Suspense, lazy } from "react";
 import { Loading } from './features/lazy-loading/Loading';
-import {PasswordRecoveryProvider} from './features/password-recovery/hooks/PasswordRecoveryContext'
+import {PasswordRecoveryProvider} from './features/password-recovery/hooks/PasswordRecoveryContext';
+import { Analytics } from '@vercel/analytics/react';
 const Login = lazy(() => import("./features/login/pages/login"));
 const Register = lazy(() => import("./features/register/pages/Register"));
 const DashboardOrganizer = lazy(() => import("./features/main-menu-organizer/pages/MenuOrganizer"));
@@ -19,23 +20,26 @@ const TicketDetails = lazy(() => import("./features/show-tickets/pages/TicketDet
 function App() {
 
   return (
-    <Suspense fallback={<Loading/>}>
-        <Routes>
-          <Route path="/" element={<Login />}/>
-          <Route path="/sign-in" element={<Register />}/>
-          <Route path="/dashboard-organizer" element={<DashboardOrganizer/>}/>
-          <Route path="/dashboard-organizer/event-creation" element={<EventCreation/>}/>
-          <Route path="/dashboard-organizer/event-edition" element={<EventEdition />}/>
-          <Route path="/dashboard-attendee" element={<DashboardAttendee/>}/>
-          <Route path="/dashboard-attendee/event-details/:eventName" element={<EventSaleDetails />} />
-          <Route path="/dashboard-attendee/payment-reservation" element={<ReservationPayment />}/>
-          <Route path="/dashboard-attendee/payment-reservation/completion" element={<Completion />}/>
-          <Route path="/password-recovery" element={<PasswordRecoveryProvider><PasswordRecovery/></PasswordRecoveryProvider>} />
-          <Route path="/dashboard-attendee/my-tickets" element={<MyTickets/>}/>
-          <Route path="/dashboard-attendee/my-tickets/:ticketId" element={<TicketDetails/>}/>
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-    </Suspense>
+    <>
+      <Suspense fallback={<Loading/>}>
+          <Routes>
+            <Route path="/" element={<Login />}/>
+            <Route path="/sign-in" element={<Register />}/>
+            <Route path="/dashboard-organizer" element={<DashboardOrganizer/>}/>
+            <Route path="/dashboard-organizer/event-creation" element={<EventCreation/>}/>
+            <Route path="/dashboard-organizer/event-edition" element={<EventEdition />}/>
+            <Route path="/dashboard-attendee" element={<DashboardAttendee/>}/>
+            <Route path="/dashboard-attendee/event-details/:eventName" element={<EventSaleDetails />} />
+            <Route path="/dashboard-attendee/payment-reservation" element={<ReservationPayment />}/>
+            <Route path="/dashboard-attendee/payment-reservation/completion" element={<Completion />}/>
+            <Route path="/password-recovery" element={<PasswordRecoveryProvider><PasswordRecovery/></PasswordRecoveryProvider>} />
+            <Route path="/dashboard-attendee/my-tickets" element={<MyTickets/>}/>
+            <Route path="/dashboard-attendee/my-tickets/:ticketId" element={<TicketDetails/>}/>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+      </Suspense>
+      <Analytics />
+    </>
   )
 }
 
